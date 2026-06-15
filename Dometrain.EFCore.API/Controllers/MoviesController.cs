866bafd5bfdf94228d1fe48397ecee8504779abb
charliegdev <charliegdev@gmail.com>
2026-06-14 19:47:27 -0700

    Initial commit, and setting up basic DB context.

---
+++ Dometrain.EFCore.API/Controllers/MoviesController.cs
@@ -0,0 +1,49 @@
+using Dometrain.EFCore.API.Data;
+using Dometrain.EFCore.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dometrain.EFCore.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MoviesController(MoviesContext moviesContext) : Controller
+{
+    [HttpGet]
+    [ProducesResponseType(typeof(List<Movie>), StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetAll()
+    {
+        return Ok(await moviesContext.Movies.ToListAsync());
+    }
+
+    [HttpGet("{id:int}")]
+    [ProducesResponseType(typeof(Movie), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> Get([FromRoute] int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(Movie), StatusCodes.Status201Created)]
+    public async Task<IActionResult> Create([FromBody] Movie movie)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPut("{id:int}")]
+    [ProducesResponseType(typeof(Movie), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> Update([FromRoute] int id, [FromBody] Movie movie)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpDelete("{id:int}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> Remove([FromRoute] int id)
+    {
+        throw new NotImplementedException();
+    }
+}
