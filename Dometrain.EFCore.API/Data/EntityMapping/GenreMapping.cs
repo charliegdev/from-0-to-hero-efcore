8003755ef222bdf787c550a6ee295c76c29e2421
charliegdev <charliegdev@gmail.com>
2026-07-01 15:05:05 -0700

    Add a new migration.

--- Dometrain.EFCore.API/Data/EntityMapping/GenreMapping.cs
+++ Dometrain.EFCore.API/Data/EntityMapping/GenreMapping.cs
@@ -10,18 +10,5 @@ public class GenreMapping : IEntityTypeConfiguration<Genre>
     public void Configure(EntityTypeBuilder<Genre> builder)
     {
         builder.Property<DateTime>("CreatedDate").HasValueGenerator<CreatedDateGenerator>();
-
-        builder.HasData(
-            new Genre { Id = 1, Name = "Action" },
-            new Genre { Id = 2, Name = "Comedy" },
-            new Genre { Id = 3, Name = "Drama" },
-            new Genre { Id = 4, Name = "Horror" },
-            new Genre { Id = 5, Name = "Sci-Fi" },
-            new Genre { Id = 6, Name = "Romance" },
-            new Genre { Id = 7, Name = "Thriller" },
-            new Genre { Id = 8, Name = "Animation" },
-            new Genre { Id = 9, Name = "Documentary" },
-            new Genre { Id = 10, Name = "Fantasy" }
-        );
     }
 }
